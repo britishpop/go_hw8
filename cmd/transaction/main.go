5586acaf3c81a9ea0e53f22de3b66bc3351ac515
britishpop <drmakc@gmail.com>
2020-07-21 16:30:50 +0300

    Удалены отладочные принты

--- cmd/transaction/main.go
+++ cmd/transaction/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"go_hw_8/pkg/transaction"
 	"os"
-	"time"
 )
 
 func main() {
@@ -16,8 +14,4 @@ func main() {
 	if errE := transaction.ImportCSV("example_import.csv"); errE != nil {
 		os.Exit(10)
 	}
-
-	d := time.Date(2020, time.January, 2, 11, 15, 10, 0, time.UTC)
-	a := d.Format(time.RFC3339)
-	fmt.Println(a)
 }
